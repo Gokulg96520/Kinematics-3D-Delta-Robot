004ebd1ab1c18062f0c111198794751ac0958538
GokulG <g.gopal@beckhoff.com>
2024-10-21 16:15:25 +0530

    VS Base Project

---
+++ 3D Delta Kinematics VS/3D Delta Kinematics VS/MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace _3D_Delta_Kinematics_VS
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl = new OpenTK.GLControl();
+            this.SuspendLayout();
+            // 
+            // glControl
+            // 
+            this.glControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.glControl.BackColor = System.Drawing.Color.Black;
+            this.glControl.Location = new System.Drawing.Point(211, 0);
+            this.glControl.Name = "glControl";
+            this.glControl.Size = new System.Drawing.Size(589, 450);
+            this.glControl.TabIndex = 0;
+            this.glControl.VSync = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.glControl);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl;
+    }
+}
+
---
+++ 3D Delta Kinematics VS/3D Delta Kinematics VS/MainForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace _3D_Delta_Kinematics_VS
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeGLComponent();
+
+        }
+
+        private void InitializeGLComponent()
+        {
+            glControl.Load += GLControl_Load;
+            glControl.Paint += GLControl_Paint;
+            glControl.Resize += GLControl_Resize;
+
+        }
+        private void GLControl_Load(object sender, EventArgs e)
+        {
+
+            // Enable blending for transparency
+            GL.Enable(EnableCap.DepthTest);
+
+            // Set the clear color
+            GL.ClearColor(Color.FromArgb(0, 0, 0, 0)); // Fully transparent red
+        }
+
+        private void GLControl_Paint(object sender, PaintEventArgs e)
+        {
+            // Clear the color and depth buffers
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            // Add your rendering code here
+
+
+            // Swap the buffers to display the rendered image
+            glControl.SwapBuffers();
+        }
+
+        private void GLControl_Resize(object sender, EventArgs e)
+        {
+            GL.Viewport(0, 0, glControl.Width, glControl.Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.MatrixMode(MatrixMode.Modelview);
+
+            //This will call GLControl_Paint
+            glControl.Invalidate();
+        }
+
+
+    }
+}
