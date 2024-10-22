ed4ffc6c8be6150d545457c4e6600543cbd396e8
GokulG <g.gopal@beckhoff.com>
2024-10-22 09:26:01 +0530

    Line Draw,  xyz translation of Drawn Line

--- 3D Delta Kinematics VS/3D Delta Kinematics VS/MainForm.Designer.cs
+++ 3D Delta Kinematics VS/3D Delta Kinematics VS/MainForm.Designer.cs
@@ -30,6 +30,12 @@ namespace _3D_Delta_Kinematics_VS
         private void InitializeComponent()
         {
             this.glControl = new OpenTK.GLControl();
+            this.zCameraMovement = new System.Windows.Forms.TrackBar();
+            this.xCameraMovement = new System.Windows.Forms.TrackBar();
+            this.yCameraMovement = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.zCameraMovement)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xCameraMovement)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yCameraMovement)).BeginInit();
             this.SuspendLayout();
             // 
             // glControl
@@ -44,21 +50,65 @@ namespace _3D_Delta_Kinematics_VS
             this.glControl.TabIndex = 0;
             this.glControl.VSync = false;
             // 
+            // zCameraMovement
+            // 
+            this.zCameraMovement.BackColor = System.Drawing.Color.Black;
+            this.zCameraMovement.CausesValidation = false;
+            this.zCameraMovement.Location = new System.Drawing.Point(12, 10);
+            this.zCameraMovement.Minimum = -10;
+            this.zCameraMovement.Name = "zCameraMovement";
+            this.zCameraMovement.Size = new System.Drawing.Size(141, 45);
+            this.zCameraMovement.TabIndex = 1;
+            this.zCameraMovement.Tag = "";
+            this.zCameraMovement.ValueChanged += new System.EventHandler(this.zCameraMovement_ValueChanged);
+            // 
+            // xCameraMovement
+            // 
+            this.xCameraMovement.BackColor = System.Drawing.Color.Black;
+            this.xCameraMovement.CausesValidation = false;
+            this.xCameraMovement.Location = new System.Drawing.Point(12, 61);
+            this.xCameraMovement.Minimum = -10;
+            this.xCameraMovement.Name = "xCameraMovement";
+            this.xCameraMovement.Size = new System.Drawing.Size(141, 45);
+            this.xCameraMovement.TabIndex = 1;
+            this.xCameraMovement.ValueChanged += new System.EventHandler(this.xCameraMovement_ValueChanged);
+            // 
+            // yCameraMovement
+            // 
+            this.yCameraMovement.BackColor = System.Drawing.Color.Black;
+            this.yCameraMovement.CausesValidation = false;
+            this.yCameraMovement.Location = new System.Drawing.Point(12, 112);
+            this.yCameraMovement.Minimum = -10;
+            this.yCameraMovement.Name = "yCameraMovement";
+            this.yCameraMovement.Size = new System.Drawing.Size(141, 45);
+            this.yCameraMovement.TabIndex = 1;
+            this.yCameraMovement.ValueChanged += new System.EventHandler(this.yCameraMovement_ValueChanged);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.yCameraMovement);
+            this.Controls.Add(this.xCameraMovement);
+            this.Controls.Add(this.zCameraMovement);
             this.Controls.Add(this.glControl);
             this.Name = "MainForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.zCameraMovement)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xCameraMovement)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yCameraMovement)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private OpenTK.GLControl glControl;
+        private System.Windows.Forms.TrackBar zCameraMovement;
+        private System.Windows.Forms.TrackBar xCameraMovement;
+        private System.Windows.Forms.TrackBar yCameraMovement;
     }
 }
 
--- 3D Delta Kinematics VS/3D Delta Kinematics VS/MainForm.cs
+++ 3D Delta Kinematics VS/3D Delta Kinematics VS/MainForm.cs
@@ -15,6 +15,10 @@ namespace _3D_Delta_Kinematics_VS
 {
     public partial class MainForm : Form
     {
+        private float _zCameraTranslation = -1.0f;
+        private float _xCameraTranslation = 0.0f;
+        private float _yCameraTranslation = 0.0f;
+
         public MainForm()
         {
             InitializeComponent();
@@ -35,32 +39,75 @@ namespace _3D_Delta_Kinematics_VS
             // Enable blending for transparency
             GL.Enable(EnableCap.DepthTest);
 
-            // Set the clear color
-            GL.ClearColor(Color.FromArgb(0, 0, 0, 0)); // Fully transparent red
+            // Set the clear color to black
+            GL.ClearColor(Color.FromArgb(0, 0, 0, 0));
         }
 
         private void GLControl_Paint(object sender, PaintEventArgs e)
         {
-            // Clear the color and depth buffers
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-
             // Add your rendering code here
+            Render();
 
-
-            // Swap the buffers to display the rendered image
-            glControl.SwapBuffers();
         }
 
         private void GLControl_Resize(object sender, EventArgs e)
         {
-            GL.Viewport(0, 0, glControl.Width, glControl.Height);
-            GL.MatrixMode(MatrixMode.Projection);
-            GL.MatrixMode(MatrixMode.Modelview);
+            if (glControl.Width > 0 && glControl.Height > 0)
+            {
 
-            //This will call GLControl_Paint
-            glControl.Invalidate();
+                GL.Viewport(0, 0, glControl.Width, glControl.Height);
+
+                // Set up a perspective projection matrix
+                GL.MatrixMode(MatrixMode.Projection);
+                GL.LoadIdentity();
+                Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(
+                    MathHelper.PiOver4, // 45-degree field of view
+                    (float)glControl.Width / glControl.Height, // Aspect ratio
+                    0.1f, // Near clipping plane
+                    100.0f // Far clipping plane
+                );
+                GL.LoadMatrix(ref perspective);
+                GL.MatrixMode(MatrixMode.Modelview);
+
+            }
         }
 
+        private void Render()
+        {
 
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit); // Clear the color and depth buffers
+
+            GL.LoadIdentity();  // Reset transformations
+            // Move the line into view along the Z-axis
+            
+            GL.Translate(_xCameraTranslation, _yCameraTranslation, _zCameraTranslation);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(1.0f, 0.0f, 0.0f);  // Set line color to red
+            GL.Vertex3(-1.0f, 0.0f, 0.0f);  // Start point of the line
+            GL.Vertex3(1.0f, 0.0f, 0.0f);   // End point of the line
+            GL.End();
+
+            glControl.SwapBuffers();  // Swap buffers to display the rendered line
+
+
+            
+
+        }
+
+        private void zCameraMovement_ValueChanged(object sender, EventArgs e)
+        {
+            _zCameraTranslation = zCameraMovement.Value;
+            glControl.Invalidate();
+        }
+        private void xCameraMovement_ValueChanged(object sender, EventArgs e)
+        {
+            _xCameraTranslation = xCameraMovement.Value;
+            glControl.Invalidate();
+        }
+        private void yCameraMovement_ValueChanged(object sender, EventArgs e)
+        {
+            _yCameraTranslation = yCameraMovement.Value;
+            glControl.Invalidate();
+        }
     }
 }
