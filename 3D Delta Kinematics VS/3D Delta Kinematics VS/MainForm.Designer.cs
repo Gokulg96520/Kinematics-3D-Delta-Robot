471f207c2c7062f8b95d23fb6eeca428de278694
GokulG <g.gopal@beckhoff.com>
2024-10-22 17:26:33 +0530

    Added Camera Movement btn

--- 3D Delta Kinematics VS/3D Delta Kinematics VS/MainForm.Designer.cs
+++ 3D Delta Kinematics VS/3D Delta Kinematics VS/MainForm.Designer.cs
@@ -30,6 +30,11 @@ namespace _3D_Delta_Kinematics_VS
         private void InitializeComponent()
         {
             this.glControl = new OpenTK.GLControl();
+            this.btnMoveLeft = new System.Windows.Forms.Button();
+            this.btnMoveRight = new System.Windows.Forms.Button();
+            this.btnMoveUp = new System.Windows.Forms.Button();
+            this.btnMoveDown = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // glControl
@@ -40,25 +45,96 @@ namespace _3D_Delta_Kinematics_VS
             this.glControl.BackColor = System.Drawing.Color.Black;
             this.glControl.Location = new System.Drawing.Point(254, 2);
             this.glControl.Name = "glControl";
-            this.glControl.Size = new System.Drawing.Size(525, 470);
+            this.glControl.Size = new System.Drawing.Size(545, 490);
             this.glControl.TabIndex = 0;
             this.glControl.VSync = false;
+            // 
+            // btnMoveLeft
+            // 
+            this.btnMoveLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMoveLeft.AutoSize = true;
+            this.btnMoveLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoveLeft.Location = new System.Drawing.Point(682, 457);
+            this.btnMoveLeft.Name = "btnMoveLeft";
+            this.btnMoveLeft.Size = new System.Drawing.Size(35, 35);
+            this.btnMoveLeft.TabIndex = 1;
+            this.btnMoveLeft.Text = "←";
+            this.btnMoveLeft.UseVisualStyleBackColor = true;
+            // 
+            // btnMoveRight
+            // 
+            this.btnMoveRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMoveRight.AutoSize = true;
+            this.btnMoveRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoveRight.Location = new System.Drawing.Point(723, 457);
+            this.btnMoveRight.Name = "btnMoveRight";
+            this.btnMoveRight.Size = new System.Drawing.Size(35, 35);
+            this.btnMoveRight.TabIndex = 1;
+            this.btnMoveRight.Text = "→";
+            this.btnMoveRight.UseVisualStyleBackColor = true;
+            // 
+            // btnMoveUp
+            // 
+            this.btnMoveUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMoveUp.AutoSize = true;
+            this.btnMoveUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoveUp.Location = new System.Drawing.Point(764, 377);
+            this.btnMoveUp.Name = "btnMoveUp";
+            this.btnMoveUp.Size = new System.Drawing.Size(35, 35);
+            this.btnMoveUp.TabIndex = 1;
+            this.btnMoveUp.Text = "↑";
+            this.btnMoveUp.UseVisualStyleBackColor = true;
+            // 
+            // btnMoveDown
+            // 
+            this.btnMoveDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMoveDown.AutoSize = true;
+            this.btnMoveDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoveDown.Location = new System.Drawing.Point(764, 417);
+            this.btnMoveDown.Name = "btnMoveDown";
+            this.btnMoveDown.Size = new System.Drawing.Size(35, 35);
+            this.btnMoveDown.TabIndex = 1;
+            this.btnMoveDown.Text = "↓";
+            this.btnMoveDown.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReset.AutoSize = true;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(764, 457);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(35, 35);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "R";
+            this.btnReset.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 494);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnMoveDown);
+            this.Controls.Add(this.btnMoveUp);
+            this.Controls.Add(this.btnMoveRight);
+            this.Controls.Add(this.btnMoveLeft);
             this.Controls.Add(this.glControl);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private OpenTK.GLControl glControl;
+        private System.Windows.Forms.Button btnMoveLeft;
+        private System.Windows.Forms.Button btnMoveRight;
+        private System.Windows.Forms.Button btnMoveUp;
+        private System.Windows.Forms.Button btnMoveDown;
+        private System.Windows.Forms.Button btnReset;
     }
 }
 
